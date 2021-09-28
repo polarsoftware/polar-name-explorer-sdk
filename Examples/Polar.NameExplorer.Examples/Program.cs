e5f2ce78e6e699edae1ab07efb3f97ae1e053ff3
Stanislav Prusac <stanislav@polarsoftware.com>
2021-09-28 21:29:49 +0200

    Hi

---
+++ Examples/Polar.NameExplorer.Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polar.NameExplorer.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
