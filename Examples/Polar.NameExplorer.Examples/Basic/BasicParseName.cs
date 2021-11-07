e744bf506c24be3e52e0e8e3c3eb0d358a8ff1ba
Stanislav Prusac <stanislav@polarsoftware.com>
2021-11-07 23:15:19 +0100

    Update BasicParseName.cs

--- Examples/Polar.NameExplorer.Examples/Basic/BasicParseName.cs
+++ Examples/Polar.NameExplorer.Examples/Basic/BasicParseName.cs
@@ -21,6 +21,21 @@ namespace Polar.NameExplorer.Examples
             Console.WriteLine($"Gender: {parseName.GenderData.Gender}");//"f"            
             Console.WriteLine($"FemalePercent: {parseName.GenderData.FemalePercent}");
             Console.WriteLine($"MalePercent: {parseName.GenderData.MalePercent}");
+            
+            Console.WriteLine("\nCountry First Name:");            
+            CountryElement countryElement = parseName.CountryData.FirstNameCountryPhrases.First().CountryElements.First();            
+            Console.WriteLine($"Country: {countryElement.Country}");
+            Console.WriteLine($"Frequency: {countryElement.Frequency}");
+            Console.WriteLine($"Certainty: {countryElement.Certainty}");
+            Console.WriteLine($"Percent: {countryElement.Percent}");
+                       
+            Console.WriteLine("\nCountry Last Name:");            
+            countryElement = parseName.CountryData.LastNameCountryPhrases.First().CountryElements.First();            
+            Console.WriteLine($"Country: {countryElement.Country}");
+            Console.WriteLine($"Frequency: {countryElement.Frequency}");
+            Console.WriteLine($"Certainty: {countryElement.Certainty}");
+            Console.WriteLine($"Percent: {countryElement.Percent}");
+            
         }        
     }
 }
